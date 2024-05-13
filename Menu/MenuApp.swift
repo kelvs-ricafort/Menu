df242aeed438d91bed0f1df12e5b76a524d07290
Kelvin <kelvs.r@icloud.com>
2024-05-13 20:36:46 +0800

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Kelvin on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
